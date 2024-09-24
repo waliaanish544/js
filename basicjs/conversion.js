fa8ea8e498fdb8d8362d7f03dcdedadcc2416d34
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-09-24 05:53:50 +0000

    conversion of dt

---
+++ basicjs/conversion.js
@@ -0,0 +1,24 @@
+// let score = 33
+
+// console.log(typeof score); //typeof is used to define what is the type of the value is
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//33 =33
+// 33abc = NAN
+// true =2;false =0
+
+let isLoggedIn = "nnn"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//0 =false
+//"" =false
+//"anish"=true
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
