e52f812fc221042162cc6a8190fa6370438e6a0c
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-09-24 05:36:37 +0000

    datatypes

---
+++ basicjs/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treates all JS code as newer version
+// alert(3+3)//we are using node js not browser
+
+// console.log(3+3);
+// console.log("anish");
+let name="anish";
+let age=18
+let isLoggedIn =false
+let state = null;
+
+
+//Primitve Datatype
+//number=>2 to pwr 53
+//bigint
+//string => ""
+//boolean => y/n
+//null => standalone value   represents empty value
+//undefined => 
+//symbol => to find the uniqueness  in any programme
+
+//object
+console.log(typeof undefined);
