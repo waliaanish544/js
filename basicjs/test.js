afeb15bea8bb0da85127789d4f7961ccf4c52595
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-09-24 05:19:48 +0000

    container config

---
+++ basicjs/test.js
@@ -0,0 +1,55 @@
+const accountId =144553
+let accountEmail ="anish1201@gmail.com"
+var accountPassword = "12345"
+accountCity = "himachal"
+
+
+console.log("accountId");
+console.table([accountId ,accountEmail, accountPassword, accountCity])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
