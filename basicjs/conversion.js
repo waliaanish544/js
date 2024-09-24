ffababebe510de1c7dc698c03d254bfb9aa560aa
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-09-24 07:39:35 +0000

    conversions

--- basicjs/conversion.js
+++ basicjs/conversion.js
@@ -11,14 +11,46 @@
 // 33abc = NAN
 // true =2;false =0
 
-let isLoggedIn = "nnn"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "nnn"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //0 =false
 //"" =false
-//"anish"=true
-let somenumber = 33
-let stringNumber = String(somenumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// //"anish"=true
+// let somenumber = 33
+// let stringNumber = String(somenumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+///////operations///////
+
+// let value =3
+// let negValue = -value;
+// console.log(negValue);
+ 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+//  let str1= "anish";
+//  let str2="walia"
+//  let str3= str1+str2
+//  console.log(str3);
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log("1"+"2");
+//  console.log("1"+2+2);
+//  console.log(1+2+"2");
+// console.log(3 + 4)* 5 % 3);
+console.log(true);
+console.log(+"");
+let num1,num2,num3;
+num1=num2=num3 =2+2
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
