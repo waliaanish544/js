d61df6d5326ceb1ded7ad31912025b198e02e9de
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-09-25 06:56:28 +0000

    datasummer

---
+++ basicjs/datasumm.js
@@ -0,0 +1,31 @@
+//Primitive Datatype
+
+//7 types: string,number,boolean,null,undefined,symbol,bigint
+
+//javascript is a 
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userNullEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId)
+const bigNumber = 3333333n
+const heros =["iron man","hulk","strange","antman"]
+let myObj= {
+    name:"anish",
+    age:2,
+}         
+const myFunction= 
+function(){
+    console.log("hello world" );
+}
+console.log(typeof myFunction);
+// refrence type datatype
+//refrence(non primitive)
+ // arrays,objects,functions
+
