6497b55dcfb7464c221d1ca7f233d27d82aa8d1c
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-09-27 06:35:49 +0000

    convsersion

--- basicjs/conversion.js
+++ basicjs/conversion.js
@@ -47,10 +47,18 @@
 //  console.log("1"+2+2);
 //  console.log(1+2+"2");
 // console.log(3 + 4)* 5 % 3);
-console.log(true);
-console.log(+"");
-let num1,num2,num3;
-num1=num2=num3 =2+2
-let gameCounter=100
-++gameCounter;
-console.log(gameCounter);
+// console.log(true);
+// console.log(+"");
+// let num1,num2,num3;
+// num1=num2=num3 =2+2
+// let gameCounter=100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let greeting="hello world";
+function sayHello(){
+    console.log("greetings");
+
+}
+sayHello();
+console.log(greeting);
