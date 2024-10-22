4f230cf04d859f0352d2b1a98aaf6e05f3c3aca8
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-10-22 07:05:34 +0000

    02_array

--- 02_basicjs/02array.js
+++ 02_basicjs/02array.js
@@ -8,9 +8,20 @@ const dc_heros = ["superman" , "flash" , "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros =[...marvel_heros, ...dc_heros]
-// console.log(all_new_heros);
+// const all_new_heros =[...marvel_heros, ...dc_heros]
+// // console.log(all_new_heros);
 
-const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("anish"))
+console.log(Array.from("anish"))
+console.log(Array.from({name:"anish"})) //intresting
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
