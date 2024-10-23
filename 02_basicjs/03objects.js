29bfd635bf415baa8f456c4fe61e5b70de5c5473
anish walia <121148015+waliaanish544@users.noreply.github.com>
2024-10-23 05:57:18 +0000

    objects

--- 02_basicjs/03objects.js
+++ 02_basicjs/03objects.js
@@ -1,1 +1,31 @@
-// singleton -->>
+// singleton 
+
+// object literals
+// Object.create //constructor method
+
+const mySym = Symbol("key1")
+
+const jsUser ={
+    name: "Anish",
+    "full name" :"anish walia",
+    age : 18,
+    [mySym]:"myKey1",
+    locatin: "himachal",
+    email: "dlp.com",
+    isLoggedIn: false,
+    LastLoginDays: ["monday","satrurday"]
+}
+// console.log(jsUser.name)
+// console.log(jsUser["email"])
+// console.log(jsUser["full name"])
+// console.log( jsUser[mySym])
+
+jsUser.email = "anish@gmail.com"
+Object.freeze ="anish@google.com"
+console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log(`helllo js user, ${this.name}`);
+}
+console.log(jsUser.greeting);
+console.log(jsUser.greetingTwo());
